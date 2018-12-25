91ae65bef7176fcdc852cc0b10b73d01d065b92a
hagrid91 <46114087+hagrid91@users.noreply.github.com>
2018-12-25 05:58:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+#What's your favorite song?
+
+#Song : Undiporaadhey
+
+Language = "Telugu"
+Singer = "Sid Sriram"
+Lyrics = "Kittu Vissapragada"
+Music = "Radhan"
+Movie = "Hushaaru"
+Director = "Sree Harsha Konuganti"
+Duration_of_song = 260 # in Seconds
+Theme = "Romantic"
+Setting = "Countryside"
+views = 3.8 # in million views
+
+print(Language)
+print(Singer)
+print(Lyrics)
+print(Music)
+print(Director)
+print(Duration_of_song)
+print(Theme)
+print(Setting)
+print(views)
+
